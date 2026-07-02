4035c97be069a9427ff2d292e0ed0d305e76ada6
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-02 20:25:26 +0530

    Organize 01_Basics folder

---
+++ 01_Basics/DataTypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch = 'A';
+    int i = 11;
+    float f = 90.99f;
+    double d = 78.67890;
+
+    cout << "Displaying R Values of all Data Types\n\n";
+
+    cout << "Character : " << ch << endl;
+    cout << "Integer : " << i << endl;
+    cout << "Float : " << f << endl;
+    cout << "Double : " << d << endl;
+
+    return 0;
+}
---
+++ 01_Basics/InputOutput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iNo = 0;
+
+    cout << "Enter number : ";
+    cin >> iNo;
+
+    cout << "Entered number is : " << iNo <<"\n";
+
+    return 0;
+}
