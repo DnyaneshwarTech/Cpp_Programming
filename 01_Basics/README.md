4d96e5b70d249ff6d4d515b0556d2d1764aa131d
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-03 11:33:46 +0530

    Add variables program in C++

---
+++ 01_Basics/README.md
@@ -0,0 +1,11 @@
+# 01_Basics
+
+This folder contains basic C++ programming examples.
+
+## Programs
+
+- FirstCppProgram.cpp
+- InputOutput.cpp
+- DataTypes.cpp
+
+Author: Dnyaneshwar Shinde
---
+++ 01_Basics/Variables.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Variable Declaration
+
+    char grade;
+    int age;
+    float marks;
+    double salary;
+   
+    // Variable Initialization
+
+    grade = 'A';
+    age = 25;
+    marks = 99.89f;
+    salary = 78.67890;
+
+    // Print Values
+
+    cout << "Grade : " << grade << endl;
+    cout << "Age : " << age << endl;
+    cout << "Marks : " << marks << endl;
+    cout << "Salary : " << salary << endl;
+
+    return 0;
+}
