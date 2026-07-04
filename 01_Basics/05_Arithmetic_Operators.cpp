3e4cc43e1672846b638c37e92d9f0da49566e147
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-05 00:24:18 +0530

    Add arithmetic operators program

---
+++ 01_Basics/05_Arithmetic_Operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 20;
+    int num2 = 10;
+
+    cout << "Addition = " << num1 + num2 << endl;
+    cout << "Subtraction = " << num1 - num2 << endl;
+    cout << "Multiplication = " << num1 * num2 << endl;
+    cout << "Division = " << num1 /  num2 << endl;
+    cout << "Modulus = " << num1 % num2 << endl;
+
+    return 0;
+}
