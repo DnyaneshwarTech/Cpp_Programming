2d4e037007ea23a0883aac794a46f688c72344b7
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-16 23:44:22 +0530

    Add if statement program in C++

---
+++ 03_Conditional_Statements/01_If.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iNo = 0;
+
+    cout<<"Enter a number : ";
+    cin>> iNo;
+
+    if(iNo > 0)
+    {
+        cout << iNo << " is a Positive Number."<< endl;
+    }
+
+    return 0;
+}
