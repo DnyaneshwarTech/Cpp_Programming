bafaaaac5b7e0d99a1e8480490b7741dc6522284
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-31 23:26:46 +0530

    Add C++ program to find sum of even numbers from 1 to N

---
+++ 04_For_Loops/08_Sum_of_Even_Numbers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iValue = 0;
+    int iSum = 0;
+    int iCnt = 0;
+
+    cout << "Enter the number : "<< endl;
+    cin >> iValue;
+
+    for(iCnt = 1; iCnt <= iValue; iCnt++)
+    {
+        if(iCnt % 2 == 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+
+    cout << "Sum of even numbers : " << iSum << endl;
+
+    return 0;
+}
