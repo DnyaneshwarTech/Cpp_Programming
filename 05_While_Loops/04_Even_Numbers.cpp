9eb2a135f418e25a96545f36d2ce4d799c06dd24
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-17 00:03:12 +0530

    Add C++ program to print even numbers using while loop

---
+++ 05_While_Loops/04_Even_Numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 2;
+
+    while(iCnt <= 20)
+    {
+        cout << iCnt << endl;
+        iCnt +=2;
+    }
+
+    return 0;
+}
