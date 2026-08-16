6262aa8ff8aaa6b5893dd4aad0e53a4a824077ce
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-16 23:55:49 +0530

    Add C++ while loop program

---
+++ 05_While_Loops/01_While_Loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 1 ;
+
+    while(iCnt <= 5)
+    {
+        cout << iCnt << endl;
+        iCnt++;
+    }
+
+    return 0;
+}
---
+++ 05_While_Loops/05_Odd_Numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 1;
+
+    while(iCnt <= 20)
+    {
+        cout << iCnt << endl;
+        iCnt +=2;
+    }
+
+    return 0;
+}
