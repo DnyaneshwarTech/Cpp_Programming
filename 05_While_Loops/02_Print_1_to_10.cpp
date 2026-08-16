87475da15686665eb6796b2a6ceb8ffdc115873e
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-16 23:57:29 +0530

    Add C++ program to print 1 to 10 using while loop

---
+++ 05_While_Loops/02_Print_1_to_10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iCnt = 1;
+
+    while(iCnt <= 10)
+    {
+        cout << iCnt << endl;
+        iCnt++;
+    }
+
+    return 0;
+}
