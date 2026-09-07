3c9e09ee996ad233316a4ceff7935036367be13e
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-09-07 21:24:43 +0530

    Add palindrome number program using while loop

---
+++ 05_While_Loops/17_Palindrome_Number.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num, originalNum, reverse = 0;
+    int digit = 0;
+
+    cout << "Enter a number : ";
+    cin >> num;
+
+    originalNum = num;
+
+    while(num > 0)
+    {
+        digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+
+    if(originalNum == reverse)
+    {
+        cout << originalNum << " is a palindrome number" << endl;
+    }
+    else
+    {
+        cout << originalNum << " is not a palindrome number" << endl;
+    }
+
+    return 0;
+}
